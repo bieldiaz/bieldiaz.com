d17b5fdd327ccd91c21c2a2e9db87910e9798484
BielDiaz <biel.diaz@gmail.com>
2020-10-20 10:42:30 +0200

    Project Page included

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,45 @@ import "../css/main.css"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
 import Jobs from "../components/Jobs"
+import Projects from "../components/Projects"
+import { graphql } from "gatsby"
 
-export default () => {
+export default ({ data }) => {
+
+  const { allStrapiProjects: { nodes: projects },
+  } = data
+
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      <Projects projects={projects} title="featured projects" showLink />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: {featured: {eq: true}}) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
